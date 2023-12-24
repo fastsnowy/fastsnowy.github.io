e1ee3b8df0050daac2ad718c76f989ef3164967c
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2023-12-24 11:43:27 +0900

    ⚡️ Update tsconfig.json, components.json, package.json, astro.config.mjs, .github/workflows/deploy.yaml, and tailwind.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,25 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import remarkObsidianCallout from "remark-obsidian-callout"
 
-import react from "@astrojs/react";
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fastsnowy.github.io',
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  site: "https://fastsnowy.github.io",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    ,
+    react(),
+  ],
   markdown: {
     syntaxHighlight: "shiki",
-  }
-});
+    remarkPlugins: [remarkObsidianCallout],
+  },
+})
